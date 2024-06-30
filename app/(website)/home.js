ac8a32e93c99b1fc871225effb0011fbf0b4d5b2
Tushar Gupta <kshitizgupta108@gmail.com>
2024-06-30 15:35:56 +0530

    updaed home

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -12,17 +12,7 @@ export default function Post({ posts }) {
       {posts && (
         <Container>
 
-          <div>
-          {/* <div className="grid gap-10 md:grid-cols-2 lg:gap-10 "> */}
-            {posts.slice(0, 2).map(post => (
-              <PostList
-                key={post._id}
-                post={post}
-                aspect="landscape"
-                preloadImage={true}
-              />
-            ))}
-          </div>
+          
           <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
             {posts.slice(2, 14).map(post => (
               <PostList key={post._id} post={post} aspect="square" />
