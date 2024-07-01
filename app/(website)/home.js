8a27fd212b1aff8ae250b3cae1bc360feef1d2a1
Tushar Gupta <kshitizgupta108@gmail.com>
2024-07-02 01:30:41 +0530

    up

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -54,67 +54,48 @@ export default function Post({ posts }) {
         </div>
       </HomeContainer>
       <HomeContainer>
-        {/* <footer>
-          <div style={{borderWidth:"3px", borderColor:"red"}}>
-            <h3>Why Choose Kozyclan?</h3>
-            <div style={{ borderWidth: "20px", borderColor: "black", height:"1000px" }}>
-              <div style={{ float: "left" }}>
-                <h4>Innovative Game Design</h4>
-                <p>
-                  Our creative team of game designers crafts original games that are easy to learn, inclusive, and endlessly entertaining. We focus on fostering laughter, teamwork, and meaningful interactions.
-                </p>
-              </div>
-              <div style={{ float: "right" }}>
-                <h4>Versatile Options</h4>
-                From icebreakers to strategy games and hilarious party activities, our diverse game collection caters to all ages and preferences. No matter the occasion, you’ll find a game that fits your theme and audience.
-
-              </div>
-            </div>
-          </div>
-        </footer> */}
         <footer class="footer" style={{ borderWidth: "30px", borderColor: "red" }}>
-        <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
-          About
-        </h1>
-        <div className="prose mx-auto mt-14 text-center dark:prose-invert">
-            <p>
+          <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
+            About
+          </h1>
+          <div className="text-center dark:prose-invert">
+            <p style={{marginLeft:"100px", marginRight:"100px", marginBottom:"40px", color:"rgba(191, 129, 52, 1)", fontWeight:"200"}}>
               At Kozyclan, we believe that every gathering is an opportunity for unforgettable fun and connection. Our mission is to transform any party, event, or casual get-together into a memorable experience with our unique, engaging social games. Whether you’re hosting a cozy family dinner, a lively birthday bash, or a corporate team-building event, Kozyclan has the perfect game to elevate the atmosphere and bring people closer together.
             </p>
-            <h3>Why Choose Kozyclan?</h3>
-            <div style={{ borderWidth: "20px", borderColor: "black" }}>
-              <div style={{ float: "left" }}>
-                <h4>Innovative Game Design</h4>
-                <p>
-                  Our creative team of game designers crafts original games that are easy to learn, inclusive, and endlessly entertaining. We focus on fostering laughter, teamwork, and meaningful interactions.
+            <h3 style={{fontSize:"30px", padding:"20px"}}>Why Choose Kozyclan?</h3>
+
+            <div class="row">
+              <div class="col-lg-6" style={{ width: "50%", float: "left", padding: "100px" }}>
+              <b style={{fontSize:"20px", color:"grey"}}>Innovative Game Design</b>
+                <p style={{ color: "rgba(91, 106, 233, 1)", fontWeight: "200" }}>                  Our creative team of game designers crafts original games that are easy to learn, inclusive, and endlessly entertaining. We focus on fostering laughter, teamwork, and meaningful interactions.
                 </p>
               </div>
-              <div style={{ float: "right" }}>
-                <h4>Versatile Options</h4>
-                From icebreakers to strategy games and hilarious party activities, our diverse game collection caters to all ages and preferences. No matter the occasion, you’ll find a game that fits your theme and audience.
-
+              <div class='col-mg-6' style={{ width: "50%", float: "left", padding: "100px" }}>
+              <b style={{fontSize:"20px", color:"grey"}}>Versatile Options</b>
+                <p style={{ color: "rgba(91, 106, 233, 1)", fontWeight: "200" }}>
+                  From icebreakers to strategy games and hilarious party activities, our diverse game collection caters to all ages and preferences. No matter the occasion, you’ll find a game that fits your theme and audience.</p>
               </div>
             </div>
-            <div>
-              <div>
-                <h4>User-Friendly Experience</h4>
-                <p>Our games come with clear instructions and minimal setup, so you can spend more time enjoying the moment and less time figuring out the rules.
+            <div class="row">
+              <div class="col-lg-6" style={{ width: "50%", float: "left", padding: "100px", paddingTop:"10px" }}>
+                <b style={{fontSize:"20px", color:"grey"}}>User-Friendly Experience</b>
+                <p style={{ color: "rgba(91, 106, 233, 1)", fontWeight: "200" }}>
+                  Our games come with clear instructions and minimal setup, so you can spend more time enjoying the moment and less time figuring out the rules.
                 </p>
               </div>
-              <div>
-
-                <h4>Community and Connection</h4>
-                <p>At the heart of Kozyclan is our commitment to building connections. We design our games to break down social barriers, spark conversations, and create lasting memories. With Kozyclan, you’re not just playing a game; you’re sharing an experience.
+              <div class='col-mg-6' style={{ width: "50%", float: "right", padding: "100px" , paddingTop:"10px"}}>
+              <b style={{fontSize:"20px", color:"grey"}}>Community and Connection</b>
+                <p style={{ color: "rgba(91, 106, 233, 1)", fontWeight: "200" }}>                  At the heart of Kozyclan is our commitment to building connections. We design our games to break down social barriers, spark conversations, and create lasting memories. With Kozyclan, you’re not just playing a game; you’re sharing an experience.
                 </p>
               </div>
             </div>
-            <div class="left-content show-up header-text wow fadeInLeft">
 
-              <div class="white-button scroll-to-section">
-                <a href="/contact">Get in touch <i class="fab fa-google-play"></i></a>
-              </div>
-            </div>
           </div>
 
+
+          <div class="white-button scroll-to-section" style={{marginLeft:"1000px"}}>
+            <a href="/contact">Get in touch <i class="fab fa-google-play"></i></a>
+          </div>
         </footer>
 
       </HomeContainer>
